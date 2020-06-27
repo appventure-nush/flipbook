23de3e7c476f529b5c0923dc063a74fd65365ca3
Lam Jun Rong <junron1@outlook.com>
2020-06-27 12:47:28 +0800

    Fix page sliding

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,17 @@
 const options = {
   pdf: "./assets/Yearbook 2019 (Web).pdf",
+  controlsProps: {
+    downloadURL: "./assets/Yearbook 2019 (Web).pdf"
+  },
   template: {
     html: './assets/templates/default-book-view.html',
     styles: [
       './assets/templates/black-book-view.css'
     ],
+    sounds: {
+      startFlip: './assets/sounds/start-flip.mp3',
+      endFlip: './assets/sounds/end-flip.mp3'
+    },
     links: [{
       rel: 'stylesheet',
       href: './node_modules/font-awesome/css/font-awesome.css'
