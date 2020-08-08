31b527112b26569d065ae7bf2e3f0266512bd238
Lam Jun Rong <junron1@outlook.com>
2020-08-08 17:57:38 +0800

    Add multiple flipbooks

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,32 @@
-const options = {
-  pdf: "./assets/Yearbook 2019 (Web).pdf",
-  controlsProps: {
-    downloadURL: "./assets/Yearbook 2019 (Web).pdf"
-  },
-  template: {
-    html: './assets/templates/default-book-view.html',
-    styles: [
-      './assets/templates/black-book-view.css'
-    ],
-    sounds: {
-      startFlip: './assets/sounds/start-flip.mp3',
-      endFlip: './assets/sounds/end-flip.mp3'
+const validFlipbooks = ["2019", "2018"]
+const flipBook = location.search.substring(1)
+if (validFlipbooks.includes(flipBook)) {
+
+  const options = {
+    pdf: `./assets/Yearbook-${flipBook}.pdf`,
+    controlsProps: {
+      downloadURL: `./assets/Yearbook-${flipBook}.pdf`
     },
-    links: [{
-      rel: 'stylesheet',
-      href: './node_modules/font-awesome/css/font-awesome.css'
-    }],
-    script: './assets/templates/default-book-view.js',
+    template: {
+      html: './assets/templates/default-book-view.html',
+      styles: [
+        './assets/templates/black-book-view.css'
+      ],
+      sounds: {
+        startFlip: './assets/sounds/start-flip.mp3',
+        endFlip: './assets/sounds/end-flip.mp3'
+      },
+      links: [{
+        rel: 'stylesheet',
+        href: './node_modules/font-awesome/css/font-awesome.css'
+      }],
+      script: './assets/templates/default-book-view.js',
+    }
+  };
+
+  const book = $("#flipbook").FlipBook(options);
+
+  var home = () => {
+    book.ctrl.goToPage(2)
   }
-};
-
-const book = $("#flipbook").FlipBook(options);
-
-var home = () => {
-  book.ctrl.goToPage(2)
 }
