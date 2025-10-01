b8aecec18fb1914f1cf52fd6ea8f01cdce294068
Walnit <wongyueheng@gmail.com>
2025-10-01 16:59:29 +0800

    Updated layout and files for 2025

--- files/assets/js/index.js
+++ files/assets/js/index.js
@@ -1,4 +1,4 @@
-const validFlipbooks = ["2021", "2020", "2019", "2018"];
+const validFlipbooks = ["2021", "2020", "15-anniversary", "2019", "2018", "10-anniversary"];
 let flipBook =
   location.search.length === 0
     ? validFlipbooks[0]
@@ -7,15 +7,24 @@ if (!validFlipbooks.includes(flipBook)) {
   flipBook = validFlipbooks[0];
 }
 
-document.title = `NUSH Year Book ${flipBook}`;
+const flipbookNames = {
+  "2021": "NUSH Yearbook 2021",
+  "2020": "NUSH Yearbook 2020",
+  "15-anniversary": "NUSH 15th Anniversary Book",
+  "2019": "NUSH Yearbook 2019",
+  "2018": "NUSH Yearbook 2018",
+  "10-anniversary": "NUSH 10th Anniversary Book"
+}
+
+document.title = flipbookNames[flipBook];
 
 // set the active navlink
 $(`#nav-${flipBook}`).addClass("active-nav-link");
 
 const options = {
-  pdf: `./assets/pdfs/Yearbook-${flipBook}.pdf`,
+  pdf: `./assets/pdfs/${flipBook}.pdf`,
   controlsProps: {
-    downloadURL: `./assets/pdfs/Yearbook-${flipBook}.pdf`,
+    downloadURL: `./assets/pdfs/${flipBook}.pdf`,
   },
   template: {
     html: "./assets/templates/default-book-view.html",
